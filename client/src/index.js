b84bb2f1656a03caaaed06e72e5d4049e435fc2a
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2023-05-02 23:01:47 -0500

    reverted index.js changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// const domNode = document.getElementById('root');
-// const root = createRoot(domNode);
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
 
-ReactDOM.render(
+root.render(
     <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+        <App />
+    </React.StrictMode>
+);
 
 serviceWorker.register();
