b19cf1f8207f97dac288cc558ebe2cbf5640079c
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2023-05-02 22:58:22 -0500

    changed structure of index.js again

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
+// import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
+// const domNode = document.getElementById('root');
+// const root = createRoot(domNode);
 
-root.render(
+ReactDOM.render(
     <React.StrictMode>
-        <App />
+      <App />
     </React.StrictMode>,
     document.getElementById('root')
-);
+  );
 
 serviceWorker.register();
